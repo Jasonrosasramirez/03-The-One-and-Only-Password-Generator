4415b2bc7627f6f8f0ab4faa44c34d72946f62b6
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-02-25 23:50:37 -0600

    got the prompts to work correctly! Having issues converting charlength to an int, but the verification loop doesn't complain. Next step is creating the password array

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // This is the element in generateBtn
 
- 
-
-
 // Write password to the #password input
 function writePassword() {
   generatePassword(); 
@@ -16,20 +13,36 @@ function writePassword() {
 function generatePassword() {
   window.alert("Hello friend. We will begin the password generation process");
   
-  var charLength = window.prompt("Select a character length between 8 and 128 inclusive");
-  // a check/function here to determine the input is numberic and between 8 and 128.    
+  //var charLength = window.prompt("Select a character length between 8 and 128 inclusive");
 
+  var charLengthBool = false; // Checks to see if the character length criteria has been met.  
+  while (charLengthBool == false) { 
+    var charLength = window.prompt("Select a character length between 8 and 128 inclusive"); // how can the input be kept as an int if a number is input. 
+    // parseInt(charLength, 10); // should change the charLength into a number but doesn't ... 
+
+    
+    if (charLength >= 8 && charLength <= 128 ) {
+      charLengthBool = true;  
+      window.alert("it worked! " + typeof(charLength));
+    } else {
+      window.alert("Woah there, partner. You need to enter a number between 8 and 128. charLength " + typeof(charLength)); 
+    }
+  }
+  
   var specialCharSelection = window.confirm("Would you like special characters to be used?\nOK for 'yes' and cancel for 'no'");
   var numericCharSelection = window.confirm("Would you like numeric characters to be used?\nOK for 'yes' and cancel for 'no'");
    // If both upper and lower are selected as false, then alphabet characters cannot be used. 
   var lowerCaseSelection = window.confirm("Would you like lowercase characters to be used?\nOK for 'yes' and cancel for 'no'");
   var upperCaseSelection = window.confirm("Would you like uppercase characters to be used?\nOK for 'yes' and cancel for 'no'");
 
-  window.alert("The selections made: " + "\n " + "\nChar Length: " + charLength + "\nSpecial: " + specialCharSelection + "\nnumeric: " + numericCharSelection + "\nlowercase: " + lowerCaseSelection + "\nuppercase: " + upperCaseSelection);
+  var readyToGen = window.confirm("Build password with this criteria? " + "\nOK for 'yes' and cancel for 'no " + "\n " + "\nChar Length: " + charLength + "\nSpecial: " + specialCharSelection + "\nNumeric: " + numericCharSelection + "\nLowercase: " + lowerCaseSelection + "\nUppercase: " + upperCaseSelection);
+  if (readyToGen === true) { 
+    window.alert("This is where the password is generated " + readyToGen);
+  } else {
+    window.alert("This is where we cancel " + readyToGen);
+  }
   
-  // the password generation array will most likely be here or triggers the new function. //window.alert(password array);
 }
-
 
 
 // Add event listener to generate button
