7299c1a0a97c0288d84a22b36d8b94cf2a2e1931
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-02-25 18:54:16 -0600

    playing with the window promts and assigning variables

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // This is the element in generateBtn
 
+ 
+
 
 // Write password to the #password input
 function writePassword() {
-  generatePassword();
+  generatePassword(); 
   
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
 function generatePassword() {
   window.alert("Hello friend. We will begin the password generation process");
-  window.prompt("Select a character length between 8 and 128 inclusive");
-  window.prompt("Select your character types to include in password"); // THEN I choose lowercase, uppercase, numeric, and/or special characters
   
-  // the password generation array will most likely be here or triggers the new function
-  
-  //window.alert(password array);
+  window.alert(charLength);
+  var charLength = window.prompt("Select a character length between 8 and 128 inclusive");
+  window.alert(charLength);
 
+
+  // window.prompt("Select your character types to include in password"); // THEN I choose lowercase, uppercase, numeric, and/or special characters
+  
+  //showMe();
+  
+  // the password generation array will most likely be here or triggers the new function. //window.alert(password array);
 }
 
-/*
-WHEN prompted for character types to include in the password
 
-
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-
-*/
+function showMe() { // this will be my debugging tool 
+  console.log("Show me function triggered"); 
+  console.log("duck equals " );
+}
 
 
 
@@ -50,8 +52,8 @@ generateBtn.addEventListener("click", writePassword);
 /* 
 24Feb2021
 . Using the chrome inspector, sources states funtion generatePassword() is undefined. 
-. 
-.
+25Feb2021
+. Finally learned to push code to Github on my own.
 .
 .
 .
