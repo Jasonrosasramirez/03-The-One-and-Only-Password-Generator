e561ca6780cbf55ba78e7cb059957d765dae27eb
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-02-25 19:40:57 -0600

    prompts are set up :) Next for the logic.

--- script.js
+++ script.js
@@ -16,25 +16,19 @@ function writePassword() {
 function generatePassword() {
   window.alert("Hello friend. We will begin the password generation process");
   
-  window.alert(charLength);
   var charLength = window.prompt("Select a character length between 8 and 128 inclusive");
-  window.alert(charLength);
+  // a check/function here to determine the input is numberic and between 8 and 128.    
 
+  var specialCharSelection = window.confirm("Would you like special characters to be used?\nOK for 'yes' and cancel for 'no'");
+  var numericCharSelection = window.confirm("Would you like numeric characters to be used?\nOK for 'yes' and cancel for 'no'");
+   // If both upper and lower are selected as false, then alphabet characters cannot be used. 
+  var lowerCaseSelection = window.confirm("Would you like lowercase characters to be used?\nOK for 'yes' and cancel for 'no'");
+  var upperCaseSelection = window.confirm("Would you like uppercase characters to be used?\nOK for 'yes' and cancel for 'no'");
 
-  // window.prompt("Select your character types to include in password"); // THEN I choose lowercase, uppercase, numeric, and/or special characters
-  
-  //showMe();
+  window.alert("The selections made: " + "\n " + "\nChar Length: " + charLength + "\nSpecial: " + specialCharSelection + "\nnumeric: " + numericCharSelection + "\nlowercase: " + lowerCaseSelection + "\nuppercase: " + upperCaseSelection);
   
   // the password generation array will most likely be here or triggers the new function. //window.alert(password array);
 }
-
-
-function showMe() { // this will be my debugging tool 
-  console.log("Show me function triggered"); 
-  console.log("duck equals " );
-}
-
-
 
 
 
