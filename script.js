ae88efaf53f154f8f73a1ad1e91b9fa803d36fab
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-02-26 21:44:31 -0600

    cleaned up the code a little. Having issue transferring information through global variables for some reason.

--- script.js
+++ script.js
@@ -1,156 +1,94 @@
-var specialChar = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
 
+// Character Arrays & Variables 
+var specialChar = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowerCase = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-var upperCase = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
-
-
-// Assignment Code
-
-
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 var generateBtn = document.querySelector("#generate"); // This is the element in generateBtn
 
-// Write password to the #password input
-function writePassword() {
-  generatePassword();
+var charLength;
+var specialCharSelection;
+var numericCharSelection;
+var lowerCaseSelection;
+var upperCaseSelection
 
-  // var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
+
+// Essential Functions 
+
 
 function generatePassword() {
   alert("Hello friend. We will begin the password generation process");
 
-  // prompt input section. Might make this into a seperate function. 
-
   var charLengthBool = false; // Checks to see if the character length criteria has been met.  
-  while (charLengthBool == false) {
-    var charLength = parseInt(prompt("Select a character length between 8 and 128 inclusive")); // how can the input be kept as an int if a number is input. 
+  while (charLengthBool === false) {
+    charLength = parseInt(prompt("Select a character length between 8 and 128 inclusive")); // how can the input be kept as an int if a number is input. 
 
     if (charLength >= 8 && charLength <= 128) {
       charLengthBool = true;
-      alert("it worked! " + typeof (charLength));
+      
+      specialCharSelection = confirm("Would you like special characters to be used?\nOK for 'yes' and cancel for 'no'");
+      numericCharSelection = confirm("Would you like numeric characters to be used?\nOK for 'yes' and cancel for 'no'");
+      // If both upper and lower are selected as false, then alphabet characters cannot be used. 
+      lowerCaseSelection = confirm("Would you like lowercase characters to be used?\nOK for 'yes' and cancel for 'no'");
+      upperCaseSelection = confirm("Would you like uppercase characters to be used?\nOK for 'yes' and cancel for 'no'");
+
     } else {
       alert("Woah there, partner. You need to enter a number between 8 and 128. charLength " + typeof (charLength));
     }
-  }
+  } // This section is the user prompt
 
-  var specialCharSelection = confirm("Would you like special characters to be used?\nOK for 'yes' and cancel for 'no'");
-  var numericCharSelection = confirm("Would you like numeric characters to be used?\nOK for 'yes' and cancel for 'no'");
-  // If both upper and lower are selected as false, then alphabet characters cannot be used. 
-  var lowerCaseSelection = confirm("Would you like lowercase characters to be used?\nOK for 'yes' and cancel for 'no'");
-  var upperCaseSelection = confirm("Would you like uppercase characters to be used?\nOK for 'yes' and cancel for 'no'");
 
+  // this triggers if the password gets generated
   var readyToGen = confirm("Build password with this criteria? " + "\nOK for 'yes' and cancel for 'no " + "\n " + "\nChar Length: " + charLength + "\nSpecial: " + specialCharSelection + "\nNumeric: " + numericCharSelection + "\nLowercase: " + lowerCaseSelection + "\nUppercase: " + upperCaseSelection);
   if (readyToGen === true) {
     alert("This is where the password is generated " + readyToGen);
+    // This is where the signal to create the password is created. 
   } else {
     alert("This is where we cancel " + readyToGen);
-  }
+  }    
 
-  // password generation section. Might make this into a seperate function. 
-
-  // Object to store key value pairs of user input 
-  var passProperties = {
+  var passwordProperties = { // Object to store key value pairs of user input 
     charLength: charLength,
     specialCharSelection: specialCharSelection,
     numericCharSelection: numericCharSelection,
     lowerCaseSelection: lowerCaseSelection,
     upperCaseSelection: upperCaseSelection
   }
-  return passProperties;
+
+  console.log("This is passwordProperties" + passwordProperties);
+  
+  return passwordProperties;
 }
 
 //Creating array or random element
-function passRanCharacters (Array) {
+function passwordRandomCharacters (Array) {
+  console.log("passwordRandomChar function Activated. char length " + charLength);
+
   var indexofArray = Math.floor(Math.random() * Array.length);
   var grabElement = Array[indexofArray];
+
+  console.log(grabElement);
 
   return grabElement;
 }
 
-// element.addEventListener(event, function, useCapture);
-generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  generatePassword();
+  passwordRandomCharacters(); 
+
+  // var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+} // Write password to the #password input. This is the passover function
+
+
+// User Actions 
+generateBtn.addEventListener("click", writePassword); // element.addEventListener(event, function, useCapture);
+
+
+
 
 
 
